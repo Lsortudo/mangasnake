751aa7729206a6e2428375f37430654ec152e3cd
Leo <leonardo.alvirubro@hotmail.com>
2025-01-31 17:34:39 -0300

    feat(auth): add JWT auth middleware and token generation

---
+++ api/middleware/middleware.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"fmt"
+	"go_mangasnake_api/api/model"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte(os.Getenv("SECRET_TOKEN"))
+
+func JWTAuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenString := ctx.GetHeader("Authorization")
+		if tokenString == "" {
+			model.ResponseJSON(ctx, http.StatusUnauthorized, "Authorization token required", nil)
+			ctx.Abort()
+			return
+		}
+
+		_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return jwtSecret, nil
+		})
+
+		if err != nil {
+			model.ResponseJSON(ctx, http.StatusUnauthorized, "invalid token", nil)
+			ctx.Abort()
+			return
+		}
+		// Token valido, proximo handler
+		ctx.Next()
+	}
+}
